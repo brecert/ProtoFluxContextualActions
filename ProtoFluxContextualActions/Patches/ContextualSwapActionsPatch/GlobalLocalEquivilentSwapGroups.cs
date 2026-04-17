65305a0870b280815d700d1c04100eb2e74727bd
IHaveAName2653 <89300269+IHaveAName2653@users.noreply.github.com>
2026-04-17 23:31:55 +1000

    hopefully fix setglobal<->setlocal transform swaps
    they seem to never order correctly. this likely works, but isnt great

--- ProtoFluxContextualActions/Patches/ContextualSwapActionsPatch/GlobalLocalEquivilentSwapGroups.cs
+++ ProtoFluxContextualActions/Patches/ContextualSwapActionsPatch/GlobalLocalEquivilentSwapGroups.cs
@@ -10,17 +10,34 @@ namespace ProtoFluxContextualActions.Patches;
 
 static partial class ContextualSwapActionsPatch
 {
-  static readonly BiDictionary<Type, Type> SetGlobalLocalEquivilents =
-    Groups.SetSlotTranformGlobalOperationGroup.Zip(Groups.SetSlotTranformLocalOperationGroup).ToBiDictionary();
+  static readonly BiDictionary<Type, Type> SetGlobalLocalEquivilents = new()
+  {
+    {typeof(SetGlobalPosition), typeof(SetLocalPosition)},
+    {typeof(SetGlobalPositionRotation), typeof(SetLocalPositionRotation)},
+    {typeof(SetGlobalRotation), typeof(SetLocalRotation)},
+    {typeof(SetGlobalScale), typeof(SetLocalScale)},
+    {typeof(SetGlobalTransform), typeof(SetLocalTransform)},
+  };
+
+  // For some reason, this seems to have the order be shuffled.
+  // static readonly BiDictionary<Type, Type> SetGlobalLocalEquivilents =
+  //   Groups.SetSlotTranformGlobalOperationGroup.Zip(Groups.SetSlotTranformLocalOperationGroup).ToBiDictionary();
 
   static readonly BiDictionary<Type, Type> GetGlobalLocalEquivilents = new()
   {
     {typeof(GlobalTransform), typeof(LocalTransform)}
   };
 
+  static readonly BiDictionary<Type, Type> GetSetTransformEquivilents = new()
+  {
+    {typeof(GlobalTransform), typeof(SetGlobalTransform)},
+    {typeof(LocalTransform), typeof(SetLocalTransform)}
+  };
+
   internal static IEnumerable<MenuItem> GlobalLocalEquivilentSwapGroups(Type nodeType)
   {
     if (TryGetSwap(SetGlobalLocalEquivilents, nodeType, out Type match)) yield return new(match);
     if (TryGetSwap(GetGlobalLocalEquivilents, nodeType, out match)) yield return new(match, connectionTransferType: ConnectionTransferType.ByIndexLossy);
+    if (TryGetSwap(GetSetTransformEquivilents, nodeType, out match)) yield return new(match);
   }
 }
