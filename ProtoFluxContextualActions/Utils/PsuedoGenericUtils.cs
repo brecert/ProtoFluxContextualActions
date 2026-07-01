3cf7c4380c56e4b0063805ea33d6037d210bba40
IHaveAName2653 <89300269+IHaveAName2653@users.noreply.github.com>
2026-07-01 14:17:40 +1000

    allow value cast to work by changing how nodes are found
    from what i know, this is fine.
    unless there is something i missed, nothing should break with this.

--- ProtoFluxContextualActions/Utils/PsuedoGenericUtils.cs
+++ ProtoFluxContextualActions/Utils/PsuedoGenericUtils.cs
@@ -18,9 +18,9 @@ public static class PsuedoGenericUtils
     GetProtoFluxNodes().Values
       .Select(t => (name: t.GetNiceTypeName(), type: t))
       .Where(a => a.name.StartsWith(startingWith) && !a.type.IsGenericType)
-      .Select(a => (a.type, ParseUnderscoreGenerics(world, a.name[startingWith.Length..])))
+      .Select(a => (a.type, ParseUnderscoreGenerics(world, a.name[startingWith.Length..]).Where(t => t != null)))
       // skip non matching
-      .Where(a => a.Item2.All(t => t != null));
+      .Where(a => a.Item2.Count() > 0);
 
   public static Type? TryGetPsuedoGenericForType(World world, string startingWith, params Type[] types)
   {
