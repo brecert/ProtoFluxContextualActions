b05a156a8cc6977e4d513e053f1091c4567819b4
IHaveAName2653 <89300269+IHaveAName2653@users.noreply.github.com>
2026-07-08 17:01:18 +1000

    more events!

--- ProtoFluxContextualActions/Patches/ContextualSwapActionsPatch/EventGroupItems.cs
+++ ProtoFluxContextualActions/Patches/ContextualSwapActionsPatch/EventGroupItems.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using ProtoFlux.Runtimes.Execution.Nodes.FrooxEngine.Slots;
 using ProtoFlux.Runtimes.Execution.Nodes.FrooxEngine.Users;
+using ProtoFlux.Runtimes.Execution.Nodes.FrooxEngine.Worlds;
 
 namespace ProtoFluxContextualActions.Patches;
 
@@ -18,13 +19,27 @@ static partial class ContextualSwapActionsPatch
     typeof(OnDeactivated),
   ];
 
+  static readonly HashSet<Type> WorldEventGroup = [
+    typeof(WorldFocused),
+    typeof(WorldUnFocused),
+    typeof(WorldSaved),
+    typeof(UserJoined),
+    typeof(UserLeft),
+    typeof(UserSpawn),
+  ];
+
   internal static IEnumerable<MenuItem> EventGroupItems(ContextualContext context)
   {
-    if (EventGroup.Contains(context.NodeType))
+    if (EventGroup.Contains(context.NodeType) || WorldEventGroup.Contains(context.NodeType))
     {
+      bool isEvent = EventGroup.Contains(context.NodeType);
       foreach (var match in EventGroup)
       {
-        yield return new MenuItem(match);
+        yield return new MenuItem(match, group: isEvent ? "" : "Events");
+      }
+      foreach (var match in WorldEventGroup)
+      {
+        yield return new MenuItem(match, group: isEvent ? "World Events" : "");
       }
     }
   }
