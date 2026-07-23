a77d35cc64245cb5b5f24f21a731aa3238a53867
bree <me@bree.dev>
2026-07-23 12:53:43 -0400

    hide nodes that can't connect to an async operation if the operation is async

--- ProtoFluxContextualActions/Patches/ContextualSelectionActionsPatch/OperationItems.cs
+++ ProtoFluxContextualActions/Patches/ContextualSelectionActionsPatch/OperationItems.cs
@@ -13,37 +13,44 @@ static partial class ContextualSelectionActionsPatch
 {
   private static IEnumerable<MenuItem> OperationMenuItems(ProtoFluxOperationProxy operationProxy)
   {
+    if (operationProxy.IsAsync.Value)
+    {
+      yield return new MenuItem(typeof(StartAsyncTask));
+      yield return new MenuItem(typeof(AsyncDynamicImpulseReceiver));
+    }
+    else
+    {
+      yield return new MenuItem(typeof(FireOnTrue));
+      yield return new MenuItem(typeof(FireOnFalse));
+      yield return new MenuItem(typeof(FireOnValueChange<bool>));
 
-    yield return new MenuItem(typeof(FireOnTrue));
-    yield return new MenuItem(typeof(FireOnFalse));
-    yield return new MenuItem(typeof(FireOnValueChange<bool>));
+      yield return new MenuItem(typeof(FireWhileTrue), group: "Loops");
+      yield return new MenuItem(typeof(SecondsTimer), group: "Loops");
+      yield return new MenuItem(typeof(Update), group: "Loops");
+      yield return new MenuItem(typeof(LocalUpdate), group: "Loops");
 
-    yield return new MenuItem(typeof(FireWhileTrue), group: "Loops");
-    yield return new MenuItem(typeof(SecondsTimer), group: "Loops");
-    yield return new MenuItem(typeof(Update), group: "Loops");
-    yield return new MenuItem(typeof(LocalUpdate), group: "Loops");
+      yield return new MenuItem(typeof(DynamicImpulseReceiver));
 
-    yield return new MenuItem(typeof(DynamicImpulseReceiver));
+      // Events are pretty useful
+      yield return new MenuItem(typeof(OnLoaded), group: "Events");
+      yield return new MenuItem(typeof(OnSaving), group: "Events");
+      yield return new MenuItem(typeof(WorldSaved), group: "World Events");
+      yield return new MenuItem(typeof(OnStart), group: "Events");
+      yield return new MenuItem(typeof(OnDuplicate), group: "Events");
+      yield return new MenuItem(typeof(OnDestroy), group: "Events");
+      yield return new MenuItem(typeof(OnDestroying), group: "Events");
+      yield return new MenuItem(typeof(OnPackageImported), group: "Events");
 
-    yield return new MenuItem(typeof(StartAsyncTask), group: "Async");
-    yield return new MenuItem(typeof(AsyncDynamicImpulseReceiver), group: "Async");
+      yield return new MenuItem(typeof(UserJoined), group: "World Events");
+      yield return new MenuItem(typeof(UserLeft), group: "World Events");
+      yield return new MenuItem(typeof(UserSpawn), group: "World Events");
 
+      yield return new MenuItem(typeof(WorldFocused), group: "Events");
+      yield return new MenuItem(typeof(WorldUnFocused), group: "Events");
 
-    // Events are pretty useful
-    yield return new MenuItem(typeof(OnLoaded), group: "Events");
-    yield return new MenuItem(typeof(OnSaving), group: "Events");
-    yield return new MenuItem(typeof(WorldSaved), group: "World Events");
-    yield return new MenuItem(typeof(OnStart), group: "Events");
-    yield return new MenuItem(typeof(OnDuplicate), group: "Events");
-    yield return new MenuItem(typeof(OnDestroy), group: "Events");
-    yield return new MenuItem(typeof(OnDestroying), group: "Events");
-    yield return new MenuItem(typeof(OnPackageImported), group: "Events");
+      yield return new MenuItem(typeof(StartAsyncTask), group: "Async");
+      yield return new MenuItem(typeof(AsyncDynamicImpulseReceiver), group: "Async");
+    }
 
-    yield return new MenuItem(typeof(UserJoined), group: "World Events");
-    yield return new MenuItem(typeof(UserLeft), group: "World Events");
-    yield return new MenuItem(typeof(UserSpawn), group: "World Events");
-
-    yield return new MenuItem(typeof(WorldFocused), group: "Events");
-    yield return new MenuItem(typeof(WorldUnFocused), group: "Events");
   }
 }
